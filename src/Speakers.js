a58a86914bda0bade43d4ba3cf87a159a2d036e2
Sergey Bolshchikov <sergey@bolshchikov.net>
2017-07-06 19:38:55 +0300

    fix(speakers): remove "keynote"

--- src/Speakers.js
+++ src/Speakers.js
@@ -72,7 +72,7 @@ class Speakers extends Component {
                         <h1>Speakers</h1>
                         <div className="container-header-flexbox">
                             <div className="container-header-info">
-                                <p>This year YGLF is hosting amazing speakers from all around the world. Listed below are our keynote speakers. For the whole lineup you can visit our Speakers page. Make sure to check back reguraly as this is just the beginning and we have many more to be announced soon!</p>
+                                <p>This year YGLF is hosting amazing speakers from all around the world. Listed below are our selected speakers. For the whole lineup you can visit our Speakers page. Make sure to check back reguraly as this is just the beginning and we have many more to be announced soon!</p>
                                 <BuyTicketsButton/>
                             </div>
                         </div>
