a62e0fa8dfc08e4c8b7a7bed4b0c87d847c21e1e
Sergey Bolshchikov <sergey@bolshchikov.net>
2017-07-06 21:20:35 +0300

    chore(index): bring sw back

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import registerServiceWorker from './registerServiceWorker';
 
 import './index.css';
 
 injectTapEventPlugin();
 
 ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
