5ffaba23a7e064fa2c9bdd221b1c7e83934ff618
Pavel Rapoport <pavel@responso.io>
2017-08-22 08:01:54 +0300

    Desktop page

--- src/pages/Workshops/index.js
+++ src/pages/Workshops/index.js
@@ -1,9 +1,43 @@
 import React, { Component } from 'react';
+import BuyTicketsButton from '../../components/BuyTicketsButton';
+import InfoBlock from '../../components/InfoBlock';
+import Workshop from '../../components/Workshop';
+import { AutoHeightFix } from '../../utils/ElementManipulation';
+import InfoBlocksData from '../../assets/data/info-blocks.json';
+import workshopsData from '../../assets/data/workshops.json';
 import './Workshops.css';
 
 class Workshops extends Component {
+  componentDidMount() {
+    AutoHeightFix(document.getElementsByClassName('auto-height-fix'));
+  }
+
   render() {
-    return <div className="workshops-page" />;
+    const workshops = workshopsData.map(workshop =>
+      <Workshop key={workshop.image_src} workshop={workshop} />
+    );
+
+    return (
+      <div className="workshops-page">
+        <div className="container">
+          <div>
+            <InfoBlock data={InfoBlocksData.workshopsPage}>
+              <div className="workshops-info">
+                <p>
+                  The workshops will take place on the &lt;DATE TBD&gt;, in the
+                  Tel-Aviv port and will be limited up to 25 participants each.
+                  Additional details will be sent at a later date.
+                </p>
+                <BuyTicketsButton />
+              </div>
+            </InfoBlock>
+          </div>
+          <div className="workshops-list">
+            {workshops}
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
