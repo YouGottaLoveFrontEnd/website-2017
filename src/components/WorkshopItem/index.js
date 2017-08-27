f6c2a317d7f272461ed13bf1f3e3fa4d347095e1
Pavel Rapoport <pavel@responso.io>
2017-08-27 08:59:44 +0300

    Menu transitions (#17)
    * Homepage workshops fixed
    
    * About page fixed

--- src/components/WorkshopItem/index.js
+++ src/components/WorkshopItem/index.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
+import { AutoHeightFix } from '../../utils/ElementManipulation';
 import './WorkShopItem.css';
 
 class WorkShopItem extends Component {
+  resize() {
+    AutoHeightFix(document.getElementsByClassName('auto-height-fix'));
+    AutoHeightFix(document.getElementsByClassName('auto-height-fix-title'));
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.resize.bind(this));
+    this.resize();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.resize.bind(this));
+  }
+
   render() {
     const { workshop } = this.props;
     return (
       <div className="workshop-container">
-        <p className="workshop-author">
+        <p className="workshop-author auto-height-fix-title">
           {workshop.first_name.toUpperCase()} {workshop.last_name.toUpperCase()}
         </p>
-        <h2 className="workshop-title">
+        <h2 className="workshop-title auto-height-fix-title">
           {workshop.title}
         </h2>
-        <p className="workshop-description">
+        <p className="workshop-description auto-height-fix">
           {workshop.text}
         </p>
       </div>
