567abf7befe581959622aa2329d3e0f97e7df606
Pavel Rapoport <pavel@responso.io>
2017-09-06 17:28:29 +0300

    Zoom in

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import Header from '../Header';
 import Footer from '../Footer';
 import Home from '../../pages/Home';
@@ -26,32 +27,42 @@ class App extends Component {
     };
   }
 
+  componentDidMount() {
+    this.appLayout = ReactDOM.findDOMNode(this.refs.appLayout);
+  }
+
   toggleMenu(isOpen) {
     this.setState({
       isOpen: isOpen,
     });
+
+    this.appLayout.style = isOpen
+      ? 'transform: scale(1.05)'
+      : 'transform: scale(1)';
   }
 
   render() {
     return (
       <Router basename={isStaging() ? '/website-2017' : '/'}>
         <ScrollToTop>
-          <div className="app">
+          <div className="app" ref="app">
             <Menu
               isOpen={this.state.isOpen}
               toggleMenu={this.toggleMenu.bind(this)}
             />
-            <Header toggleMenu={this.toggleMenu.bind(this)} />
-            <div className="app-body">
-              <Route exact path="/" component={Home} />
-              <Route path="/codeofconduct" component={CodeOfConduct} />
-              <Route path="/about" component={About} />
-              <Route path="/speakers" component={Speakers} />
-              <Route path="/workshops" component={Workshops} />
-              <Route path="/sponsors" component={Sponsors} />
-              <Route path="/schedule" component={Schedule} />
+            <div className="app-layout" ref="appLayout">
+              <Header toggleMenu={this.toggleMenu.bind(this)} />
+              <div className="app-body">
+                <Route exact path="/" component={Home} />
+                <Route path="/codeofconduct" component={CodeOfConduct} />
+                <Route path="/about" component={About} />
+                <Route path="/speakers" component={Speakers} />
+                <Route path="/workshops" component={Workshops} />
+                <Route path="/sponsors" component={Sponsors} />
+                <Route path="/schedule" component={Schedule} />
+              </div>
+              <Footer />
             </div>
-            <Footer />
           </div>
         </ScrollToTop>
       </Router>
