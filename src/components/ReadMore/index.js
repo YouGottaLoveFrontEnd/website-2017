4ce446b612849c2e393c0ae0ca473d4c96aac9dd
Pavel Rapoport <pavel@responso.io>
2017-09-06 09:33:44 +0300

    Transition added

--- src/components/ReadMore/index.js
+++ src/components/ReadMore/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { AutoHeightFix } from '../../utils/ElementManipulation';
 import './ReadMore.css';
 
 class ReadMore extends Component {
@@ -7,7 +9,45 @@ class ReadMore extends Component {
 
     this.state = {
       isOpen: false,
+      cells: props.cells || 3,
     };
+  }
+
+  resize() {
+    AutoHeightFix(document.getElementsByClassName('auto-height-fix'));
+
+    setTimeout(() => {
+      AutoHeightFix(document.getElementsByClassName('auto-height-fix-wrapper'));
+    });
+  }
+
+  componentDidMount() {
+    this.container = ReactDOM.findDOMNode(this.refs.readMoreContainer);
+    this.wrapper = ReactDOM.findDOMNode(this.refs.readMoreWrapper);
+
+    this.openHeight = this.wrapper.clientHeight;
+    this.closeHeight = this.state.cells * 45;
+
+    this.toggle(this.state.isOpen);
+  }
+
+  toggle(isOpen) {
+    this.resize();
+
+    this.setState({
+      isOpen: isOpen,
+    });
+
+    this.container.style.height =
+      (isOpen ? this.openHeight : this.closeHeight) + 'px';
+  }
+
+  close() {
+    this.toggle(false);
+  }
+
+  open() {
+    this.toggle(true);
   }
 
   render() {
@@ -15,7 +55,7 @@ class ReadMore extends Component {
 
     if (this.props.paragraphs.length > 0) {
       paragraphs = this.props.paragraphs.map(paragraph =>
-        <p>
+        <p className="auto-height-fix">
           {paragraph}
         </p>
       );
@@ -23,12 +63,21 @@ class ReadMore extends Component {
 
     return (
       <div className="read-more">
-        <div className="read-more-wrapper">
-          {paragraphs}
+        <div className="read-more-container" ref="readMoreContainer">
+          <div
+            className="read-more-wrapper auto-height-fix-wrapper"
+            ref="readMoreWrapper"
+          >
+            {paragraphs}
+          </div>
         </div>
         {this.state.isOpen
-          ? <a className="btn-underline">Read less</a>
-          : <a className="btn-underline">Read more</a>}
+          ? <a className="btn-underline" onClick={this.close.bind(this)}>
+              Read less
+            </a>
+          : <a className="btn-underline" onClick={this.open.bind(this)}>
+              Read more
+            </a>}
       </div>
     );
   }
