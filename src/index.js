67304a00f8417e7c3a0e71909413f1f3ec05da30
Sergey Bolshchikov <sergey@bolshchikov.net>
2017-09-07 11:18:26 +0300

    chore(index): restore sw

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import registerServiceWorker from './registerServiceWorker';
 
 injectTapEventPlugin();
 
 ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
